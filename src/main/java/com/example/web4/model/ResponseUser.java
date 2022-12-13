1c6d7d48062a39ab989bfa93585aa6bfa30b9e67
Larisa Mozgovaya <95574250+laramozg@users.noreply.github.com>
2022-12-13 20:41:37 +0300

    212

--- src/main/java/com/example/web4/model/ResponseUser.java
+++ src/main/java/com/example/web4/model/ResponseUser.java
@@ -2,21 +2,19 @@ package com.example.web4.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @AllArgsConstructor
 
-public class ResponseDTO {
-    public ResponseDTO(int statusCode, String message) {
+public class ResponseUser {
+    public ResponseUser(int statusCode, String message) {
         this.statusCode = statusCode;
         this.message = message;
     }
 
-    public ResponseDTO(int statusCode, String accessToken, String refreshToken) {
+    public ResponseUser(int statusCode, String accessToken, String refreshToken) {
         this.statusCode = statusCode;
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
--- src/main/java/com/example/web4/service/UserService.java
+++ src/main/java/com/example/web4/service/UserService.java
@@ -3,12 +3,11 @@ package com.example.web4.service;
 import com.example.web4.exceptions.IncorrectUserCredentialsException;
 import com.example.web4.jwt.JwtTokenProvider;
 import com.example.web4.exceptions.UsernameExistException;
-import com.example.web4.model.ResponseDTO;
+import com.example.web4.model.ResponseUser;
 import com.example.web4.model.User;
 import com.example.web4.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +18,7 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public ResponseDTO registration (String username, String password) {
+    public ResponseUser registration (String username, String password) {
         userCredentialsValidation(username,password);
         User user = User.builder()
                 .username(username)
@@ -29,17 +28,17 @@ public class UserService {
             throw new UsernameExistException("Данное имя пользователя занято!");
         }
         userRepository.save(user);
-        return new ResponseDTO(HttpStatus.CREATED.value(),"Регистрация прошла успешно" );
+        return new ResponseUser(HttpStatus.CREATED.value(),"Регистрация прошла успешно" );
     }
 
-    public ResponseDTO authorization (String username, String password) {
+    public ResponseUser authorization (String username, String password) {
         User user = userRepository.findByUsername(username);
         String token = jwtTokenProvider.createToken(username);
         if (user == null) {
             throw new IncorrectUserCredentialsException("Неправильный логин или пароль!");
         }
         if (passwordEncoder.matches(password,user.getPassword())) {
-            return new ResponseDTO(HttpStatus.OK.value(), token,"");
+            return new ResponseUser(HttpStatus.OK.value(), token,"");
         } else {
             throw new IncorrectUserCredentialsException("Неправильный логин или пароль!");
 
